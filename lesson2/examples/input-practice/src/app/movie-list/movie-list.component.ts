0dff5c9d3d1a0e871e2d621f0b08855736ff149d
StephNaum <you@example.com>
2021-04-11 16:18:05 -0500

    bonus completed

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -13,8 +13,29 @@ export class MovieListComponent implements OnInit {
    ngOnInit() {
    }
    addMovie (newTitle: string) {
-      if (! this.movies.includes(newTitle) && newTitle !== ''){
+      let message= '';
+      if (newTitle === ''){
+         message= `Please provide a title`;
+      } else if (this.movies.includes(newTitle)){
+         message= `${newTitle} is already in the list`;
+      } else {
          this.movies.push(newTitle);
       }
+      return message;
    }
+   // The following code is one solution to the bonus task.
+   // Comment out lines 16 - 20, then uncomment the following lines
+   
+   /*addMovie (newTitle: string) {
+      let errorMsg = '';
+      if(newTitle === ''){
+         errorMsg = 'Please enter a movie title.';
+      } else if (this.movies.includes(newTitle)) {
+         errorMsg = `${newTitle} is already in your movie list.`;
+      } else {
+         this.movies.push(newTitle);
+      }
+      return errorMsg;
+   }*/
+   
 }
