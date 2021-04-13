b736c3d705501cbb41b2e710e567be6d47b42483
StephNaum <you@example.com>
2021-04-13 14:00:45 -0500

    lesson3 material

--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -8,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class SkillSetComponent implements OnInit {
    listHeading: string = 'Some Coding Skills I Know';
    skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
-   alternateColor: string = 'black';
-   bulletType: string = 'A';
-   changeColor: boolean = true;
+   alternateColor: string = 'purple';
+   bulletType: string = 'i';
+   changeColor: boolean = false;
+   //reversed: string = 'reversed';
+   //reversed: boolean = true;
 
    constructor() { }
 
